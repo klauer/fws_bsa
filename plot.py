de37d6a53ff314a3f39583e61d79a865f3558ff4
K Lauer <kenneth.lauer@gmail.com>
2017-07-11 12:34:17 -0700

    Tweak reading further

--- plot.py
+++ plot.py
@@ -22,69 +22,83 @@ class Header(ctypes.LittleEndianStructure):
 
 
 class EntryBase(ctypes.LittleEndianStructure):
+    _fields_ = [('data', ctypes.c_uint32 * 3)]
+
     @property
-    def value(self):
-        # all entries are using only the lower 32-bits
-        return self.data[2]
+    def raw_value(self):
+        return ((self.data[1] << 16) | self.data[0] >> 16)
+
+
+def sign_extend(value, *, bits=18):
+    sign_bit = 1 << (bits - 1)
+    return (value & (sign_bit - 1)) - (value & sign_bit)
 
 
 class UIntEntry(EntryBase):
-    _fields_ = [('data', ctypes.c_uint32 * 3)]
+    bits = 18
+
+    @property
+    def raw_mask(self):
+        return (1 << self.bits) - 1
+
+    @property
+    def value(self):
+        return self.raw_value & self.raw_mask
 
 
 class IntEntry(EntryBase):
-    _fields_ = [('data', ctypes.c_int32 * 3)]
+    bits = 18
+
+    @property
+    def value(self):
+        return sign_extend(self.raw_value, bits=self.bits)
 
 
 class FloatEntry(EntryBase):
-    _fields_ = [('data', ctypes.c_float * 3)]
+    @property
+    def fixed(self):
+        return bool(self.data[0] & (1 << 15))
+
+    @property
+    def n(self):
+        return self.data[0] & 0x1fff
+
+    @property
+    def mean(self):
+        if self.fixed:
+            return float(self.raw_value)
+
+        exception_condition = (self.data[0] & (1 << 13))
+        n = self.n
+        if not exception_condition and n:
+            raw_val = ((self.data[1] & 0xffff) << 16) | (self.data[0] >> 16)
+            return float(raw_val) / n
+
+    @property
+    def value(self):
+        return self.mean
 
 
-class Entry(ctypes.LittleEndianStructure):
-    _fields_ = [('data', ctypes.c_uint32 * 3)]
-    repr_type = 'raw'
-
-    def as_pulse_id(self):
-        return (self.data[2] << 32) | self.data[1]
-
-    def as_raw(self):
-        return ((self.data[1] << 16) |
-                (self.data[0] >> 16)
-                )
-
-    def as_uint96(self):
-        return ((self.data[2] << 64) |
-                (self.data[1] << 32) |
-                (self.data[0])
-                )
-
-    def __repr__(self):
-        if self.repr_type == 'pulse_id':
-            return repr(self.as_pulse_id())
-        elif self.repr_type == 'float':
-            return repr(self.as_float())
-        elif self.repr_type == 'raw':
-            return repr(self.as_raw())
-        else:
-            return repr(self.as_uint96())
+class UnknownEntry(EntryBase):
+    ...
 
 
 class WireBSAData(ctypes.LittleEndianStructure):
     _pack_ = 1
     _fields_ = [('header', Header),
-                ('speed', UIntEntry),
+                ('speed_freq', UIntEntry),
                 ('lower_limit', UIntEntry),
-                ('signed_speed', IntEntry),
-                ('position', UIntEntry),
+                ('signed_speed_freq', IntEntry),
+                ('position', IntEntry),
                 ('pmt_integer', UIntEntry),
                 ('pmt_float', FloatEntry),
                 ('status', UIntEntry),
-                ('remaining_entries', Entry * (31 - 7)),
+                ('remaining_entries', UnknownEntry * (31 - 7)),
                 ]
 
-    named_entries = ['speed',
+    named_entries = ['speed_freq',
                      'lower_limit',
-                     'signed_speed',
+                     'signed_speed_freq',
                      'position',
                      'pmt_integer',
                      'pmt_float',
@@ -109,21 +123,19 @@ def read_file(fn, *, cls=WireBSAData):
             f.readinto(data)
             assert data.header.nchan == 31
 
-            info['nchan'].append(data.header.nchan)
             header = data.header
             for name in cls.named_entries:
                 info[name].append(getattr(data, name).value)
-            info['pulse_id_lower'].append(header.pulse_id_lower)
-            info['pulse_id_upper'].append(header.pulse_id_upper)
+            for name in ('nchan', 'pulse_id_lower', 'pulse_id_upper'):
+                info['pulse_id_lower'].append(getattr(header, name))
             info['pulse_id'].append((header.pulse_id_upper << 32) |
                                     header.pulse_id_lower)
 
     return info
 
 
-def sign_extend(value, *, bits=18):
-    sign_bit = 1 << (bits - 1)
-    return (value & (sign_bit - 1)) - (value & sign_bit)
+def running_mean(x, n):
+    return np.convolve(x, np.ones((n, )) / n)[(n - 1):]
 
 
 def plot(fn):
@@ -139,9 +151,15 @@ def plot(fn):
     plt.ylabel('Position [um]')
 
     plt.figure()
-    # plt.plot(np.asarray(data['speed']))
-    plt.plot([sign_extend(speed, bits=18) for speed in data['speed']])
-    plt.title('Speed')
+    speed_freq = np.asarray(data['speed_freq'])
+    # speed in um/s, calculated by number of 156MHz clock pulses between
+    # encoder pulses
+    speed = 1. / (speed_freq * 6.4e-9)
+    # speed = running_mean(speed, 20)
+    plt.plot(speed)
+    plt.xlabel('Data point')
+    plt.ylabel('Speed [um/s]')
+    plt.title('BSA-Acquired Speed vs Data Point')
     plt.show()
 
 
